e50bfe94af13ba25b7419d463cd38eacad8a41a8
Agang31 <agangqanxwa@gmail.com>
2026-04-18 02:41:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("JavaScript is connected!");
 const toggleButton = document.getElementById('theme-toggle'); 
 toggleButton.addEventListener('click', function() { 
     document.body.classList.toggle('dark-mode'); 
+    document.querySelector('header').classList.toogle('dark-mode');
+    document.querySelector('footer').classList.toogle('dark-mode');
     if (document.body.classList.contains('dark-mode')) { 
         toggleButton.textContent = 'Light Mode'; 
     } else { 
