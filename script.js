7024169ed5336f74b0b23175449d54dfaa030e6b
Agang31 <agangqanxwa@gmail.com>
2026-04-24 12:54:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ console.log("JavaScript is connected!");
 const toggleButton = document.getElementById('theme-toggle'); 
 toggleButton.addEventListener('click', function() { 
     document.body.classList.toggle('dark-mode'); 
-    document.querySelector('header').classList.toogle('dark-mode');
-    document.querySelector('footer').classList.toogle('dark-mode');
+
     if (document.body.classList.contains('dark-mode')) { 
         toggleButton.textContent = 'Light Mode'; 
     } else { 
         toggleButton.textContent = 'Dark Mode'; 
     } 
 }); 
+
+window.onload = function() {
+    if (!localStorage.getItem('cookiesAccepted')) {
+        document.getElementById('cookie-banner').style.display = 'block';
+    }
+}
